d5c68bb96209b743422f51295eea4d15ac098873
merhasmukh <mhgn0001@gmail.com>
2024-11-17 11:48:35 +0530

    fix models path issue

--- app.py
+++ app.py
@@ -26,8 +26,8 @@ async def predict(text: str = Form(...)):
     criminal_info =  [input_text]
 
     # Load the models
-    sub_category_model = joblib.load('./latest_models/second_approach/sub_category_model.pkl')
-    category_model = joblib.load('./latest_models/second_approach/category_model.pkl')
+    sub_category_model = joblib.load('./utils/latest_models/second_approach/sub_category_model.pkl')
+    category_model = joblib.load('./utils/latest_models/second_approach/category_model.pkl')
     sub_category_prediction = sub_category_model.predict(criminal_info)
     print(f"Sub-Category {sub_category_prediction}")
     predicted_category = category_model.predict(sub_category_prediction)
@@ -45,8 +45,8 @@ async def predict(text: str = Form(...)):
             summary="Predict Category and Subcategory",
             description="""This endpoint takes a piece of text and predicts the category and subcategory based on the trained model.
                             \n
-                            Category Accuracy: \n
-                            Sub-Category Accuracy: \n
+                            Category Accuracy: 82.04% \n
+                            Sub-Category Accuracy: 82.04% \n
                             replace the string with your text input in given request body.
                         """)          
 async def cyber_crime_classify(text: str = Form(...)):
